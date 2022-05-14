6b911272f6685988832ab5cae0e1894aaaca72b5
Nan Xiang <chrisxiang86@hotmail.com>
2022-05-14 19:08:46 -0400

    Updates

---
+++ precache-manifest.2d07aef2a6741b7c08a198188dcb3348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937900f7223197592e3796b28fc47ceb",
+    "url": "/menuetest/index.html"
+  },
+  {
+    "revision": "1db75db920ceaf92c769",
+    "url": "/menuetest/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menuetest/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menuetest/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db75db920ceaf92c769",
+    "url": "/menuetest/static/js/main.5158d193.chunk.js"
+  },
+  {
+    "revision": "e2409a62a4bd8ac08313",
+    "url": "/menuetest/static/js/runtime-main.a28ab40b.js"
+  }
+]);
